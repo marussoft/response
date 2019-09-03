b814c01c5b2d638a3c188716b886898ac8c44306
milinsky <mail@milinsky.ru>
2019-09-03 16:39:26 +1000

    dev branch

---
+++ src/Response.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marussia\Response;
+
+class Response
+{
+    protected $content;
+
+    public function __construct()
+    {
+    
+    }
+    
+    public function view()
+    {
+    
+    }
+    
+    public function json()
+    {
+    
+    }
+    
+    public function header()
+    {
+    
+    }
+    
+    public function code()
+    {
+    
+    }
+    
+    public function send()
+    {
+
+    }
+}
