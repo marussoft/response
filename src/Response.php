79af29bec9af3d3ff04ee081fd1f3e73341d0b61
milinsky <mail@milinsky.ru>
2019-09-03 18:07:51 +1000

    send content

--- src/Response.php
+++ src/Response.php
@@ -157,6 +157,34 @@ class Response
         return $this;
     }
     
+    public function sendContent()
+    {
+        echo $this->content;
+        return $this;
+    }
+    
+    public function sendHeaders()
+    {
+        // headers have already been sent by the developer
+        if (headers_sent()) {
+            return $this;
+        }
+        // headers
+        foreach ($this->headers->allPreserveCaseWithoutCookies() as $name => $values) {
+            $replace = 0 === strcasecmp($name, 'Content-Type');
+            foreach ($values as $value) {
+                header($name.': '.$value, $replace, $this->statusCode);
+            }
+        }
+        // cookies
+        foreach ($this->headers->getCookies() as $cookie) {
+            header('Set-Cookie: '.$cookie->getName().strstr($cookie, '='), false, $this->statusCode);
+        }
+        // status
+        header(sprintf('HTTP/%s %s %s', $this->version, $this->statusCode, $this->statusText), true, $this->statusCode);
+        return $this;
+    }
+    
     public function send()
     {
 
